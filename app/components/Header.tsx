4e503380d34909bb1a46334aa6d0636539e35854
Vishnu Soni <tonysoni@Vishnus-MacBook-Air.local>
2026-07-05 14:21:40 +0530

    Added mobile header, and added About Section

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,8 @@
 "use client"
 
+import { ReorderThree } from "react-ionicons";
 import { useActiveSection } from "./ActiveSectionContext"
+import { useEffect, useRef, useState } from "react";
 
 const navItems = [
     {
@@ -34,8 +36,22 @@ const divTranslate = {
 
 export default function Header() {
     const { activeSection, setActiveSection } = useActiveSection();
+    const [mobileHeaderShown, setMobileHeaderShown] = useState<boolean>(false);
+    const burgerRef = useRef<HTMLButtonElement | null>(null);
+    const dropdownRef = useRef<HTMLDivElement | null>(null);
+    useEffect(() => {
+        const listener: any = window.addEventListener("click", (e: MouseEvent) => {
+            if(!burgerRef.current?.contains(e.target as Node) && !dropdownRef.current?.contains(e.target as Node)){
+                setMobileHeaderShown(false);
+            }
+        })
+
+        return () => {
+            window.removeEventListener('click', listener);
+        }
+    }, [])
     return (
-        <header className="sticky top-0 pt-16 pb-5 bg-black z-10">
+        <header className="sticky top-0 md:pt-12 md:pb-5 bg-black z-10">
             <div className="
                 justify-between items-center box-border px-4 hidden 
                 mx-auto 
@@ -65,6 +81,36 @@ export default function Header() {
                     </ul>
                     <div className={`absolute w-24 h-[2px] bg-sky-300 ${divTranslate[activeSection]} transition-all`}></div>
                 </nav>
+            </div>
+            <div className="md:hidden relative py-5 mb-5 box-border w-full px-5 flex flex-row justify-between items-center">
+                <div className="text-2xl font-bold">{"<"}VS<span className="text-sky-300">/</span>{">"}</div>
+                <button onClick={() => {setMobileHeaderShown(!mobileHeaderShown)}} ref = {burgerRef}>
+                    <ReorderThree
+                        color = "white"
+                        width = "30px"
+                        height = "30px"
+                    />
+                </button>
+                <div className={`${mobileHeaderShown? "flex" : "hidden"} absolute w-40 bg-zinc-900 top-[70%] right-5 p-3`} ref = {dropdownRef}>
+                    <ul className="flex flex-col">
+                        {
+                            navItems.map((item) => {
+                                return (
+                                    <li key = {item?.id} onClick = {() => {setActiveSection(item.id)}} className={`
+                                        hover:text-sky-300 transition-all 
+                                        lg:mx-12
+                                        md:mx-7
+                                        ${activeSection == item?.id ? "text-sky-300" : ""}
+                                    `}>
+                                        <a href = {item?.href}>
+                                            {item.name}
+                                        </a>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
             </div>
         </header>
     )
