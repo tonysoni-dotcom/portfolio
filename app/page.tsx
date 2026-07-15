5dc5c2cb70e784d717e5c5f98f2cb5ac981f4226
Vishnu Soni <tonysoni@Vishnus-MacBook-Air.local>
2026-07-16 00:16:24 +0530

    lazyImport

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
+"use client"
+
+import { lazy, Suspense, useState } from "react";
+import HomeComp from "./Home";
+const DashComp = lazy(() => import("./Dashboard"));
+
 export default function Home() {
+  const [nav, setNav] = useState('home')
   return (
     <main>
+      <div>
+        <button onClick = {() => {setNav("home")}}>Home</button>
+        <button onClick={() => {setNav("dash")}}>Dashboard</button>
+      </div>
+      {
+        nav === 'home' && <HomeComp/>
+      }
+      {
+        nav === 'dash' && 
+        <Suspense fallback = {<div>Loading Dash</div>}>
+          <DashComp/>
+        </Suspense>
+      }
     </main>
   );
 }
