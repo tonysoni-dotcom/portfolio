29da9391e9f3d44b64dc37028f70349caf991c21
Vishnu Soni <tonysoni@Vishnus-MacBook-Air.local>
2026-07-15 20:32:54 +0530

    basic

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,6 @@
-import AboutSection from "./components/AboutSection";
-import Carousel from "./components/Carousel";
-import ContactSection from "./components/ContactSection";
-import ExperiencesSection from "./components/ExperiencesSection";
-import HeroSection from "./components/HeroSection";
-import Projects from "./components/Projects";
-
-// import IntersectionObserver from "./components/intersectionObserver";
-
 export default function Home() {
   return (
-    // <main className="min-h-screen flex flex-col items-center bg-black">
     <main>
-      <HeroSection/>
-      <Carousel/>
-      <AboutSection/>
-      <ExperiencesSection/>
-      <Projects/>
-      <ContactSection/>
     </main>
   );
 }
