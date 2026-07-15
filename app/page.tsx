4c6fce10a6a9349614082590aaff0fe55fa8a03b
Vishnu Soni <tonysoni@Vishnus-MacBook-Air.local>
2026-07-15 23:31:01 +0530

    Suspense boundary

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,41 @@
-export default function Home() {
+"use client"
+import { useState, use, Suspense, useMemo, useEffect } from 'react';
+
+function UserProfile({ userPromise }) {
+  // use() the promise here
+  // render name, email, phone
+  let userData:{name: string} = use(userPromise);
+  console.log(userData)
   return (
-    <main>
-    </main>
+    <div>
+      {userData?.name}
+    </div>
+  )
+}
+
+export default function App() {
+  // useState lazy init to hold the promise
+  // handleRefresh creates a new promise
+  const [userPromise, setUserPromise] = useState<Promise<Object> | null>(()=>fetch("https://jsonplaceholder.typicode.com/users/1").then((resp) => resp.json()))
+
+  const [count, setCount] = useState(0);
+  // const userPromise = fetch("https://jsonplaceholder.typicode.com/users/1").then(r => r.json());
+  const handleRefresh = () => {
+    // setUserPromise(fetch("https://jsonplaceholder.typicode.com/users/1").then((resp) => resp.json()));
+  }
+
+  // const userPromise = useMemo(() => {
+  //   return fetch("https://jsonplaceholder.typicode.com/users/1").then((resp) => resp.json());
+  // }, [])
+  
+  return (
+    <>
+      <button onClick={() => setCount(c => c + 1)}>Trigger re-render ({count})</button>
+      <button onClick={handleRefresh}>Refresh</button>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div>heheheh</div>
+        <UserProfile userPromise={userPromise} />
+      </Suspense>
+    </>
   );
 }
